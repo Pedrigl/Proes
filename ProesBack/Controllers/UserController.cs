4979f04b529708946f4e46b35752d63bd0530003
Pedro <pedrogabrielcruzlima@gmail.com>
2023-10-13 15:42:42 -0300

    authorizar controller de user

--- ProesBack/Controllers/UserController.cs
+++ ProesBack/Controllers/UserController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using ProesBack.Domain.Entities;
 using ProesBack.Interfaces;
 
@@ -6,6 +7,7 @@ namespace ProesBack.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+    [Authorize]
     public class UserController : ControllerBase
     {
         private readonly ILoginViewModelService _loginViewModelService;
