dcf609b32200b8fd4224ca1871b8959eadfb7964
Pedro <pedrogabrielcruzlima@gmail.com>
2023-10-31 00:11:57 -0300

    remember this

---
+++ ProesBack/Infrastructure/Data/Migrations/20231023125158_loginchange.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProesBack.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class loginchange : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "UserType",
+                table: "User");
+
+            migrationBuilder.AddColumn<int>(
+                name: "TokenExpiration",
+                table: "Login",
+                type: "int",
+                nullable: true);
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserType",
+                table: "Login",
+                type: "int",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "TokenExpiration",
+                table: "Login");
+
+            migrationBuilder.DropColumn(
+                name: "UserType",
+                table: "Login");
+
+            migrationBuilder.AddColumn<int>(
+                name: "UserType",
+                table: "User",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+    }
+}
---
+++ ProesBack/Infrastructure/Data/Migrations/20231023131254_correction.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ProesBack.Infrastructure.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class correction : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TokenExpiration",
+                table: "Login",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<int>(
+                name: "TokenExpiration",
+                table: "Login",
+                type: "int",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldDefaultValue: 60);
+        }
+    }
+}
