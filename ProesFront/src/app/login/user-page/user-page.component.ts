5120cc8586093bbbb758e35c6850f1d7d4c514ec
Pedro <pedrogabrielcruzlima@gmail.com>
2023-11-21 01:46:20 -0300

    navegação

--- ProesFront/src/app/login/user-page/user-page.component.ts
+++ ProesFront/src/app/login/user-page/user-page.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { UserRepositoryService } from '../../shared/services/user-repository.service';
-import { UserModel } from '../../Interfaces/user.model'; 
+import { UserModel } from '../../Interfaces/user.model';
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-user-page',
   templateUrl: './user-page.component.html',
   styleUrls: ['./user-page.component.css']
 })
 export class UserPageComponent {
-  constructor(private userRepository: UserRepositoryService) {
+  constructor(private userRepository: UserRepositoryService, private router: Router) {
   }
 
   user!: UserModel
@@ -16,6 +17,8 @@ export class UserPageComponent {
     this.userRepository.createUser(this.user)
       .subscribe(res => {
         this.user = res;
+
+        this.router.navigateByUrl('/home');
       },
         err => {
           console.log(err);
