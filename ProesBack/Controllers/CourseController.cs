21539c12a5e146dfdb250eb1b1e34c8498578d21
Pedro <pedrogabrielcruzlima@gmail.com>
2024-02-05 15:21:37 -0300

    Controller de matéria

--- ProesBack/Controllers/CourseController.cs
+++ ProesBack/Controllers/CourseController.cs
@@ -30,7 +30,7 @@ namespace ProesBack.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest();
+                return BadRequest(ex.Message);
             }
         }
 
@@ -44,7 +44,7 @@ namespace ProesBack.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest();
+                return BadRequest(ex.Message);
             }
         }
 
@@ -66,7 +66,7 @@ namespace ProesBack.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest();
+                return BadRequest(ex.Message);
             }
         }
 
@@ -88,7 +88,7 @@ namespace ProesBack.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest();
+                return BadRequest(ex.Message);
             }
         }
 
@@ -107,7 +107,7 @@ namespace ProesBack.Controllers
             }
             catch (Exception ex)
             {
-                return BadRequest();
+                return BadRequest(ex.Message);
             }
         }
     }
